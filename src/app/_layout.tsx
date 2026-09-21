57412b06b9c7cb5c5e6b334071bfa9f8b73c252f
Bartosz Janiuk <bartosz.janiuk@tsh.io>
2026-09-21 14:38:02 +0200

    feat: integrate with nativewind, tanstack query

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,12 @@
 import { Stack } from "expo-router";
+import "../../global.css";
+
+import AppProviders from "@/providers/AppProviders";
 
 export default function RootLayout() {
-  return <Stack />;
+	return (
+		<AppProviders>
+			<Stack />
+		</AppProviders>
+	);
 }
